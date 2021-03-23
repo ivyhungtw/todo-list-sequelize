a687bed2976c2e39cc517a56831ecffd9e817721
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-23 15:26:19 -0700

    feat: add detail page

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 // Require modules
 const home = require('./modules/home')
 const users = require('./modules/users')
+const todos = require('./modules/todos')
 
 // Direct request to modules
+router.use('/todos', todos)
 router.use('/users', users)
 router.use('/', home)
 
---
+++ routes/modules/todos.js
@@ -0,0 +1,21 @@
+// Require Express and Express router
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
+// Set up routes
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const todo = await Todo.findByPk(id)
+    res.render('detail', { todo: todo.toJSON() })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// Export
+module.exports = router
