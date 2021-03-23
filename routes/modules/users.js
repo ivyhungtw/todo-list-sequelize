988e7fd0ccd78322925c991ce8c4609b7373416a
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-23 11:35:12 -0700

    feat: add relations between user and todo models

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
 // Set up routes
 router.get('/login', (req, res) => {
   res.render('login')
@@ -15,8 +19,10 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => {
-  res.send('register')
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  await User.create({ name, email, password })
+  res.redirect('/')
 })
 
 router.get('/logout', (req, res) => {
