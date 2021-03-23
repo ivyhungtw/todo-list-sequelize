e2ea6144d87db4b7bc6cbb4d269246a82ffc371a
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-23 16:38:29 -0700

    feat: authenticate user in todos and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  authenticator: (req, res, next) => {
+    // User must login before accessing other pages
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/users/login')
+  },
+}
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
 
+const { authenticator } = require('../middleware/auth')
+
 // Direct request to modules
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 // Export
 module.exports = router
