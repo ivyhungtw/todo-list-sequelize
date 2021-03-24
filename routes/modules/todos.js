c3873c779f6938e75671ab5516f46d7b80c4179f
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-23 20:44:11 -0700

    feat: add update todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -32,5 +32,30 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+router.get('/:id/edit', async (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  const todo = await Todo.findOne({ where: { id, UserId } })
+  res.render('edit', { todo: todo.toJSON() })
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const id = req.params.id
+    const { name, isDone } = req.body
+
+    let todo = await Todo.findOne({ where: { id, UserId } })
+    todo.name = name
+    todo.isDone = isDone === 'on'
+    await todo.save()
+
+    res.redirect(`/todos/${id}`)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 // Export
 module.exports = router
