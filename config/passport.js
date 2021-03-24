47299854f1749c46c9685cb043cd59cf9f78aa8e
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-23 20:59:10 -0700

    feat: add facebook login feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 
 const db = require('../models')
@@ -38,6 +39,41 @@ module.exports = app => {
       }
     )
   )
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName'],
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const { name, email } = profile._json
+          const user = await User.findOne({ where: { email } })
+
+          if (user) return done(null, user)
+
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+
+          const salt = await bcrypt.genSalt(10)
+          const hash = await bcrypt.hash(randomPassword, salt)
+          await User.create({
+            name,
+            email,
+            password: hash,
+          })
+
+          return done(null, user)
+        } catch (err) {
+          done(err, false)
+        }
+      }
+    )
+  )
+
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 // Direct request to modules
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 // Export
