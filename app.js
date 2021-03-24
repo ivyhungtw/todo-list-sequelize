c300e51163ea4445ac0e15517becc85d591fbd6e
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-23 17:30:57 -0700

    feat: add message partials

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
-
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const flash = require('connect-flash')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -27,9 +27,12 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,10 +7,14 @@ const Todo = db.Todo
 const User = db.User
 
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // Set up routes
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login', {
+    email: req.session.email,
+    password: req.session.password,
+  })
 })
 
 router.post(
@@ -27,24 +31,52 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const emailRule = /^\w+((-\w+)|(\.\w+)|(\+\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/
+  const errors = []
+  // Before creating an account,
+  // make sure all the required fields are correct
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'Please fill out all fields.' })
+  }
+  if (email.search(emailRule) === -1) {
+    errors.push({ message: 'Please enter the correct email address.' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'Password and confirmPassword do not match.' })
+  }
+  if (errors.length > 0) {
+    return res.render('register', {
+      name,
+      email,
+      password,
+      confirmPassword,
+      errors,
+    })
+  }
 
   try {
-    // Create an account if the email has not been used
+    // make sure email has not been used yet
     const user = await User.findOne({ where: { email } })
 
     if (user) {
-      console.log('User already exists')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword,
-      })
+      req.session.email = email
+      req.flash(
+        'warning_msg',
+        'A user with this email already exists. Choose a different address or login directly.'
+      )
+      return res.redirect('/users/register')
     }
 
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
     await User.create({ name, email, password: hash })
+
+    // Show registered email and success message on login page
+    req.session.email = email
+    req.flash(
+      'success_msg',
+      `${req.body.email} register successfully! Please login.`
+    )
 
     res.redirect('/')
   } catch (error) {
@@ -54,6 +86,9 @@ router.post('/register', async (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', 'Logout successfully!')
+  req.session.email = ''
+  req.session.password = ''
   res.redirect('/users/login')
 })
 
